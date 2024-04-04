aa695adec1431020c196235bcac1c22dd2dc03b1
G3remy <20222117@itla.edu.do>
2024-04-03 22:51:28 -0400

    IdentityContext done.

---
+++ SedisBackend.Infrastructure.Identity/Contexts/IdentityContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SedisBackend.Infrastructure.Identity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SedisBackend.Infrastructure.Identity.Contexts
+{
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //FLUENT API
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("Identity");
+
+            modelBuilder.Entity<ApplicationUser>(entity =>
+            {
+                entity.ToTable(name: "Users");
+            });
+
+            modelBuilder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable(name: "Roles");
+            });
+
+            modelBuilder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable(name: "UserRoles");
+            });
+
+            modelBuilder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable(name: "UserLogins");
+            });
+        }
+    }
+}
---
+++ SedisBackend.Infrastructure.Identity/IOC/ServiceRegistration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SedisBackend.Core.Application.Interfaces.Services;
+using SedisBackend.Infrastructure.Identity.Contexts;
+using SedisBackend.Infrastructure.Identity.Entities;
+using SedisBackend.Infrastructure.Identity.Services;
+
+namespace SedisBackend.Infrastructure.Identity.IOC
+{
+    public static class ServiceRegistration
+    {
+        public static void IdentityLayerRegistration(this IServiceCollection service, IConfiguration configuration)
+        {
+            #region Context
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                service.AddDbContext<IdentityContext>(options => options.UseInMemoryDatabase("InMemoryIdentity"));
+            }
+            else
+            {
+                service.AddDbContext<IdentityContext>(options =>
+                {
+                    options.UseSqlServer(configuration.GetConnectionString("IdentityConnection"),
+                    m => m.MigrationsAssembly(typeof(IdentityContext).Assembly.FullName));
+                });
+            }
+            #endregion
+
+            #region Services
+
+
+            service.AddIdentity<ApplicationUser, IdentityRole>()
+              .AddEntityFrameworkStores<IdentityContext>().AddDefaultTokenProviders();
+
+            service.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/User";
+                options.AccessDeniedPath = "/User/AccessDenied";
+            });
+
+            service.AddAuthentication();
+
+            #endregion
+
+            service.AddTransient<IAccountServices, AccountServices>();
+        }
+    }
+}
