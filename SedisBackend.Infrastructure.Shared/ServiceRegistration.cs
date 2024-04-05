c54f9f8c4f6e3de2a53ff42f38dea598d01d561a
Yahinniel Alejandro Torres Vásquez <20221996@itla.edu.do>
2024-04-05 12:10:50 -0500

    Agregando el servicio de email.

---
+++ SedisBackend.Infrastructure.Shared/ServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SedisBackend.Core.Application.Interfaces.Services;
+using SedisBackend.Core.Domain.Settings;
+using SedisBackend.Infrastructure.Shared.Services;
+
+namespace SedisBackend.Infrastructure.Shared
+{
+    public static class ServiceRegistration
+    {
+        public static void AddSharedInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<MailSettings>(configuration.GetSection("MailSettings"));
+            services.AddTransient<IEmailServices, EmailService>();
+        }
+    }
+}
