2f4e7e1cca441903affcc21a3dd1191ad1bcf517
ElRoy14 <roylinrodriguez5@gmail.com>
2024-04-07 10:04:32 -0400

    Adding Migrations

---
+++ WebApi/Controllers/v1/Appointment/AppointmentController.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SedisBackend.Core.Application.Dtos.Domain_Dtos.Appointments;
+using SedisBackend.Core.Application.Interfaces.Services.Domain_Services.Appointments;
+using SedisBackend.WebApi.Controllers.v1;
+
+namespace WebApi.Controllers.v1.Appointment
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentController : BaseApiController
+    {
+        private readonly IAppointmentService _appointmentsService;
+
+        public AppointmentController(IAppointmentService appointmentService)
+        {
+            _appointmentsService = appointmentService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BaseAppointmentDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> GetAllAppointments()
+        {
+            try
+            {
+                var appointments = await _appointmentsService.GetAllAsync();
+
+                if (appointments == null || appointments.Count == 0)
+                {
+                    return NotFound();
+                }
+
+                return Ok(appointments);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BaseAppointmentDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> GetAppointment(int id)
+        {
+            try
+            {
+                var appointments = await _appointmentsService.GetByIdAsync(id);
+
+                if (appointments == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(appointments);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> CreateAppointment(SaveAppointmentDto appointment)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                await _appointmentsService.AddAsync(appointment);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BaseAppointmentDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> UpdateAppointment(int id, SaveAppointmentDto appointment)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                await _appointmentsService.UpdateAsync(appointment, id);
+                return Ok(appointment);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPatch]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BaseAppointmentDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> PatchAppointment(int id, SaveAppointmentDto appointment)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                await _appointmentsService.UpdateAsync(appointment, id);
+                return Ok(appointment);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> DeleteAppointment(int id, SaveAppointmentDto appointment)
+        {
+            try
+            {
+
+                await _appointmentsService.Delete(id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+    }
+}
