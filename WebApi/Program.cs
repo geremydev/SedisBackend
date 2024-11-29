c53cb2aae24586d3fd8b0c3bafcc3e437d53796e
layla <20198368@itla.edu.do>
2024-11-29 19:30:55 -0400

    Image

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -17,7 +17,7 @@ using WebApi.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Linux character scaping is shitt
+// Linux character scaping is shit image
 LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
 builder.Services.ConfigureCors();
